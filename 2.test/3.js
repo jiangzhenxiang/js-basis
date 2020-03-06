24e76cc59aa7c617e35e81f80cfb2a6d4672e3b8
姜振祥 <jiangzx@jiedaibao.com>
2020-03-06 17:38:37 +0800

    js 基础

--- 2.test/3.js
+++ 2.test/3.js
@@ -7,9 +7,13 @@
 
 Function.prototype.apply = function (obj, args) {
     const context = obj;
+    // 增加一个临时函数名（symbol保证唯一性）
     const fn = Symbol();
+    // this是函数本身，当前函数
     context[fn] = this;
+    // 执行当前函数
     const result = context[fn](...args);
+    // 删除函数
     delete context[fn];
     return result;
 }
@@ -19,20 +23,20 @@ Function.prototype.call = function (obj, ...args) {
     const result = obj[xxx](...args);//fn2.call()
     return result;
 }
-function fn1() { console.log(1); }
-function fn2() { console.log(2); }//2
+// function fn1() { console.log(1); }
+// function fn2() { console.log(2); }//2
 // fn1.call.call(fn2);  // 2  相当于fn2.call()执行
 // fn2 = this
 //fn1.call.call.call.call.call.call.
-call.call(fn2);
+// call.call(fn2);
 //第一次的时间   this=call  context=fn2
 //fn2.call
 //this=fn2 context=window window.fn2();
 
 
-/* function sum(amount1, amount2) {
+function sum(amount1, amount2) {
     console.log(this.age + amount1 + amount2);
 }
 let obj = { age: 10 };
 sum.call(obj, 20, 30);
-sum.apply(obj, [20, 30]); */
+// sum.apply(obj, [20, 30]);
